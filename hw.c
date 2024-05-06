fd0bd38fd35e7e98e1db44d8a1aa803d92e7896a
Louis <u11202229@o365.ttu.edu.tw>
2024-05-07 00:21:39 +0800

    test

---
+++ hw.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	int code,x,pig,A,a,b,c;
+	printf("-------------------------\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("| E1B   29   張祿鷾     |\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("|       .     .         |\n");
+	printf("|                       |\n");
+	printf("|          >            |\n");
+	printf("|       -------         |\n");
+	printf("|       |     |         |\n");
+	printf("|       -------         |\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("-------------------------\n");
+	printf("請輸入密碼");
+	scanf("%d",&code);
+	for(;;)
+	{
+	if(x==3)
+	{
+		system("pause");
+		return 0;
+	}
+	if(code==2024)
+	break;
+	x++;
+	printf("密碼錯誤%d次\n",x);
+	scanf("%d",&code);
+	
+	
+	
+	
+}
+system("cls");
+printf("--------------------------\n");
+printf("|a. 畫出三角形            |\n");
+printf("|b. 顯示乘法表            |\n");
+printf("|c .結束                  |\n");
+printf("--------------------------\n");
+scanf("%s",&pig);
+if(pig==A)
+
+
+
+system("cls");
+if(pig==a)
+printf("sds");
+system("pause");
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
