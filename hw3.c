ffc3a51eb518876caf123f95f3da550fc26f949c
Louis <u11202229@o365.ttu.edu.tw>
2024-05-20 21:56:17 +0800

    新增標頭檔

---
+++ hw3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("-------------------------\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("| E1B   29   ±i¸SøÝ     |\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("|       .     .         |\n");
+	printf("|                       |\n");
+	printf("|          >            |\n");
+	printf("|       -------         |\n");
+	printf("|       |     |         |\n");
+	printf("|       -------         |\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("|                       |\n");
+	printf("-------------------------\n");
+	system("pause");
+	return 0;
+	
+	
+	
+ } 
